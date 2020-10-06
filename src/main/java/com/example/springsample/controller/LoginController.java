4d64b77482cca3f922697e73511e4c901c982009
tomohiro.abe <tomohiro52523@gmail.com>
2020-10-06 14:37:26 +0900

    LoginControllerクラスのMappingアノテーション指定誤りを修正

--- src/main/java/com/example/springsample/controller/LoginController.java
+++ src/main/java/com/example/springsample/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.springsample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -14,7 +15,7 @@ public class LoginController {
     }
 
     // ログイン画面のPOST用コントローラー
-    @GetMapping("/login")
+    @PostMapping("/login")
     public String postLogin(Model model){
         // login.htmlへ画面遷移
         return "login/login";
