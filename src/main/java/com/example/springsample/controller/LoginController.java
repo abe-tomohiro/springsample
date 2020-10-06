43036ce87acb05cee03459f75763ced7a45da1c7
阿部知弘 <tomohiro52523@gmail.com>
2020-10-06 11:01:39 +0900

    Add login Page and Controller

---
+++ src/main/java/com/example/springsample/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.springsample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    // ログイン画面のGET用コントローラー
+    @GetMapping("/login")
+    public String getLogin(Model model){
+        // login.htmlへ画面遷移
+        return "login/login";
+    }
+
+    // ログイン画面のPOST用コントローラー
+    public String postLogin(Model model){
+        // login.htmlへ画面遷移
+        return "login/login";
+    }
+
+}
