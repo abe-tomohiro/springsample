c4382ee0cbb82d8200521e48926f66514bbd8aa7
tomohiro.abe <tomohiro52523@gmail.com>
2020-10-07 11:17:41 +0900

    Validationエラーメッセージを独自キー紐付け方式に変更＋フォームクラスとメッセージを紐付けるWebConfigを追加

---
+++ src/main/java/com/example/springsample/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.springsample;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        // メッセージのプロパティファイル名（デフォルト）を指定します。
+        // 下記ではmessages.propertiesファイルがセットされる。
+        bean.setBasename("classpath:messages");
+        // メッセージプロパティの文字コードを指定
+        bean.setDefaultEncoding("UTF-8");
+        return bean;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean(){
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource());
+        return localValidatorFactoryBean;
+    }
+}
--- src/main/java/com/example/springsample/domain/model/SignupForm.java
+++ src/main/java/com/example/springsample/domain/model/SignupForm.java
@@ -10,32 +10,32 @@ import java.util.Date;
 public class SignupForm {
 
     // 必須入力＋メールアドレス形式
-    @NotBlank
-    @Email
+    @NotBlank(message = "{require_check}")
+    @Email(message = "{email_check")
     private String userId;      // ユーザーID
 
     // 必須入力、文字列長：4-100桁まで、半角英数字のみ
-    @NotBlank
-    @Size(min = 4, max = 100)
-    @Pattern(regexp="^[a-zA-Z0-9]+$")
+    @NotBlank(message = "{require_check}")
+    @Size(min = 4, max = 100, message = "{length_check}")
+    @Pattern(regexp="^[a-zA-Z0-9]+$", message = "{pattern_check}")
     private String password;    // パスワード
 
     // 必須入力
-    @NotBlank
+    @NotBlank(message = "{require_check}")
     private String userName;    // ユーザー名
 
     // 必須入力＋日付形式
-    @NotNull
+    @NotNull(message = "{require_check}")
     @DateTimeFormat(pattern="yyyy/MM/dd")
     private Date birthday;      // 誕生日
 
     // 値が20-100まで
-    @Min(20)
-    @Max(100)
+    @Min(value = 20, message = "{min_check}")
+    @Max(value = 100, message = "{max_check}")
     private int age;            // 年齢
 
     // falseのみ可能
-    @AssertFalse
+    @AssertFalse(message = "{false_check}")
     private boolean marriage;   // 結婚ステイタス
 
 }
